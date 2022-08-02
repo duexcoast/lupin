1a572788c62496a9fba848a14706a3481833cd08
uxconor <conor.ux@gmail.com>
2022-08-02 14:22:24 -0400

    plugged in mongodb and created User Model Class

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.MongoURI);
 
 const app = express();
 
