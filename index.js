fe3afa8719a4bfc2d0adea194fa63c171ef224be
uxconor <conor.ux@gmail.com>
2022-08-02 11:05:03 -0400

    refactored index, implemented directory structure/separation of concerns

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-// new instance of Google Strategy with passport with config.
-// passport
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    // This function runs when
-    // This runs
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  })
-);
-
-// User is returning from Google OAuth with an authentication code.
-app.get('/auth/google/callback', passport.authenticate('google'));
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+module.exports = (app) => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email'],
+    })
+  );
+
+  // User is returning from Google OAuth with an authentication code.
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
