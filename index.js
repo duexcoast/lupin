a05bfbe0c018d625e056435b580f644a3eee6536
uxconor <conor.ux@gmail.com>
2022-08-02 00:49:14 -0400

    finished first half of google oauth with passport

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 // new instance of Google Strategy with passport with config.
-// passport 
-passport.use(new GoogleStrategy());
+// passport
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    // This function runs when
+    // This runs
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token: ', accessToken);
+      console.log('refresh token: ', refreshToken);
+      console.log('profile: ', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+// User is returning from Google OAuth with an authentication code.
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('listening');
 });
-
