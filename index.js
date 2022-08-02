82a85fe77ad620bba46f396922a2fccb47bed426
uxconor <conor.ux@gmail.com>
2022-08-01 21:58:01 -0400

    installed passport

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// new instance of Google Strategy with passport with config.
+// passport 
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('listening');
 });
+
