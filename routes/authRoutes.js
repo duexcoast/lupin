0cdd6d2e9ec43091030ddeee6d1d1a15653fe6f6
uxconor <conor.ux@gmail.com>
2022-08-03 13:25:19 -0400

    added logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,19 @@ module.exports = (app) => {
   // User is returning from Google OAuth with an authentication code.
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    // this logout fn is attached to the request object automatically by passport
+    // it takes the cookie that contains our user's id, and kills the id that's in there.
+    // passport v0.6 now implements logout() as an async fn, that you can use for error handling.
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+    });
+    // this will send the user object back, which should be undefined.
+    res.send(req.user);
+  });
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
