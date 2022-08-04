ffe61d740b17ee1d70e45a8d547ba6215113b9ef
uxconor <conor.ux@gmail.com>
2022-08-04 16:15:39 -0400

    added CRA and proxy middleware

--- services/passport.js
+++ services/passport.js
@@ -38,3 +38,15 @@ passport.use(
     }
   )
 );
+
+async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+
+  if (existingUser) {
+    // we already have a record with the given profile id
+    return done(null, existingUser);
+  }
+  // we don't have a user record with this ID, make a new record
+  const user = await new User({ googleId: profile.id }).save();
+  done(null, user);
+};
