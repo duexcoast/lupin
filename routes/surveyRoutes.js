33ad4a6d97feb1f6902a1b87029fbdd57d20597f
uxconor <conor.ux@gmail.com>
2022-08-16 14:39:11 -0400

    POST /api/survey accepts survey information in req.body

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,14 +1,31 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.get('/api/surveys', requireLogin, (req, res) => {
+  // returns a list of surveys created by the current user
+  app.get('/api/surveys', (req, res) => {});
+
+  // creates a new survey
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     //
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split().map((email) => ({
+        email: email.trim(),
+      })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
   });
 
-  app.post('/api/surveys', (req, res) => {
-    //
-  });
-
+  // record feedback from a user
   app.post('/api/surveys/webhook', (req, res) => {
     //
   });
